83064cc9e3779999022dc9d845e3e127ee0b58ea
Chidinma <okoromannie@gmail.com>
2023-10-13 13:41:26 -0700

    print numbers xtimes

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * more_number - prints number 0 to 14 ten times
+ * more_numbers - prints number 0 to 14 ten times
  * Return: Always 0
  */
 
@@ -15,7 +15,7 @@ void more_numbers(void)
 		for (b = 0; b < 15; b++)
 		{
 			if (b > 9)
-			_putchar('l');
+			_putchar('1');
 			_putchar(b % 10 + '0');
 		}
 		_putchar('\n');
