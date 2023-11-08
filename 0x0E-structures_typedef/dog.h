2437a836c27d265ff7b829986ed9a840439c8c58
Chidinma <okoromannie@gmail.com>
2023-11-08 04:13:16 -0800

    structs

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_STRUCT_H
+#define DOG_STRUCT_H
+
+/**
+ * struct dog - a new type
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: dog's owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
