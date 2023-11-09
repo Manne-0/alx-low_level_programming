8f990b2d3560f95fd3e0d7b4aeb96bc2d6faf2d5
Chidinma <okoromannie@gmail.com>
2023-11-09 06:22:48 -0800

    function pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ * print_name - a functio that prints name
+ *
+ * @f: variable to hold the string name
+ * @name: name of person
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+
+	f(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,8 @@
+#ifndef FUNCTION_POINTER_H
+#define FUNCTION_POINTER_H
+
+int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
+int int_index(int *array, int size, int (*cmp)(int));
+
+#endif
