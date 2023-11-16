ba7aa97913df6e7e5bd573fbddf6a6607185ffe8
Chidinma <okoromannie@gmail.com>
2023-11-16 05:48:30 -0800

    Number of elements in a linked list

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -6,7 +6,7 @@
 /**
  * list_len - returns the number of elements in a linked list
  * @h: list
- * return: number of elements
+ * Return: number of elements
  */
 size_t list_len(const list_t *h)
 {
