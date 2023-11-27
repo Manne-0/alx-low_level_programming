5de1e8d05f6650029bacb505a51d9ea653cae935
Chidinma <okoromannie@gmail.com>
2023-11-27 00:09:52 -0800

    binary to integer

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,7 +10,11 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int result = 0;
+	unsigned int result;
+
+	if (b == NULL)
+		return (0);
+	result = 0;
 
 	while (*b != '\0')
 	{
@@ -19,7 +23,7 @@ unsigned int binary_to_uint(const char *b)
 		{
 			result |= 1;
 		}
-		else if (*b != '0' || b == NULL)
+		else if (*b != '0')
 			return (0);
 
 		b++;
