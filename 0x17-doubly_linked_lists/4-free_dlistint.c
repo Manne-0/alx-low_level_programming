99b48fdcb99a179b7dc42575672b8bc130204eec
Mannie <okoromannie@gmail.com>
2024-01-11 09:45:17 -0800

    frees a doubly linked list

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_dlistint - frees a doubly linked list
+ * @head: head of the list
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *tmp;
+
+	if (head != NULL)
+		while (head->prev != NULL)
+			head = head->prev;
+
+	while ((tmp = head) !=  NULL)
+	{
+		head = head->next;
+		free(tmp);
+	}
+}
